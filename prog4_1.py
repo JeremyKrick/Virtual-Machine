d262e49470c3a1f4e69d66bcfe0caa30eb97ab21
Jeremy Krick <jeremiah.krick@gmail.com>
2018-06-23 14:14:07 -0700

    WIP: Parse

--- prog4_1.py
+++ prog4_1.py
@@ -1,13 +1,36 @@
-class Tokenize():
-    def __init__(self, str):
-        self.str = str
-        for line in str:
-            words = line.split()
-            for n, i in enumerate(words):
-                if (words[n] == "push" or words[n] == "pop" or words[n] == "add" or 
-                    words[n] == "sub" or words[n] == "mul" or words[n] == "div" or 
-                    words[n] == "mod" or words[n] == "skip" or words[n] == "save" or 
-                    words[n] == "get"): True
-                elif words[n].lstrip('-').isdigit(): True
-                else:
-                    raise ValueError("Unexpected Token: " + words[n])
+def Tokenize(str):
+        words = str.split()
+        for n, i in enumerate(words):
+            if (words[n] == "push" or words[n] == "pop" or words[n] == "add" or 
+                words[n] == "sub" or words[n] == "mul" or words[n] == "div" or 
+                words[n] == "mod" or words[n] == "skip" or words[n] == "save" or 
+                words[n] == "get"): True
+            elif words[n].lstrip('-').isdigit(): True
+            else:
+                raise ValueError("Unexpected Token: " + words[n])
+        return words
+
+def Parse(tokens):
+    for n, i in enumerate(tokens):
+        print(tokens[n])
+        if(tokens[n] == "pop" or tokens[n] == "add" or tokens[n] == "sub" or 
+            tokens[n] == "mul" or tokens[n] == "div" or tokens[n] == "mod" or 
+            tokens[n] == "skip" and tokens[n][1] == isdigit()): True
+        elif(tokens[n] == "push" or tokens[n] == "save" or tokens[n] == "get" or tokens[n].lstrip('-').isdigit()): True
+        else:
+            raise ValueError("Parse error: " + tokens[n])
+    return
+
+"""
+def parse(tokens):
+    if(isValid(tokens)): True;
+    elif(tokens[0] == "push" or tokens[0] == "save" or tokens[0] == "get" and tokens[0].lstrip('-').isdigit()): True
+    else:
+        raise ValueError("Parse error on line ")
+    return
+
+def isValid(validate):
+    return ((validate[0] == "pop" or validate[0] == "add" or validate[0] == "sub" or
+             validate[0] == "mul" or validate[0] == "div" or validate[0] == "mod" or 
+             validate[0] == "skip") and (len(validate) == 1))
+"""
--- prog4_3.py
+++ prog4_3.py
@@ -1,10 +1,15 @@
 import sys
 from prog4_1 import Tokenize
+from prog4_1 import Parse
 
 def main():
+    list = []
     with open(sys.argv[1]) as f:
-        data = f.readlines()
-    my_object = Tokenize(data)
+        for line in f:
+            valid_tokens = Tokenize(line)
+            list.append(valid_tokens)
+    for tokens in list: 
+        Parse(tokens)
 
 if __name__ == '__main__':
     main()
