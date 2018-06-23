f033bd481ddbc36bdda1fb8733b3adbc88c9f77c
Jeremy Krick <jeremiah.krick@gmail.com>
2018-06-23 15:42:23 -0700

    WIP: parsing/tokenizeing

--- prog4_1.py
+++ prog4_1.py
@@ -9,27 +9,28 @@ def Tokenize(str):
             else:
                 raise ValueError("Unexpected Token: " + words[n])
         return words
-
+"""
 def Parse(tokens):
     for n, i in enumerate(tokens):
+        #print(tokens[n])
         if(tokens[n] == "pop" or tokens[n] == "add" or tokens[n] == "sub" or 
             tokens[n] == "mul" or tokens[n] == "div" or tokens[n] == "mod" or 
             tokens[n] == "skip"): True
         elif((tokens[n] == "push" or tokens[n] == "save" or tokens[n] == "get") or tokens[n].lstrip('-').isdigit()): True
         else:
             raise ValueError("Parse error: " + tokens[n])
-    return
-
+    return tokens
 """
-def parse(tokens):
-    if(isValid(tokens)): True;
-    elif(tokens[0] == "push" or tokens[0] == "save" or tokens[0] == "get" and tokens[0].lstrip('-').isdigit()): True
+
+def Parse(tokens):
+    if(isValid(tokens)): True
+    elif(tokens[0] == "push" or tokens[0] == "save" or tokens[0] == "get" and tokens[0][1].lstrip('-').isdigit()): True
     else:
         raise ValueError("Parse error on line ")
-    return
+    return tokens
 
 def isValid(validate):
     return ((validate[0] == "pop" or validate[0] == "add" or validate[0] == "sub" or
              validate[0] == "mul" or validate[0] == "div" or validate[0] == "mod" or 
              validate[0] == "skip") and (len(validate) == 1))
-"""
+
--- prog4_3.py
+++ prog4_3.py
@@ -3,13 +3,14 @@ from prog4_1 import Tokenize
 from prog4_1 import Parse
 
 def main():
-    list = []
+    validTokens = []
     with open(sys.argv[1]) as f:
         for line in f:
-            valid_tokens = Tokenize(line)
-            list.append(valid_tokens)
-    for tokens in list: 
+            validTokens.append(Tokenize(line))
+    for tokens in validTokens: 
         Parse(tokens)
+        #print(Parse(tokens))
+        
 
 if __name__ == '__main__':
     main()
