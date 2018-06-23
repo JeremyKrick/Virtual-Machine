f3ce729dbff76734792ed72c0e46659412d8f8c7
Jeremy Krick <jeremiah.krick@gmail.com>
2018-06-23 14:32:23 -0700

    isdigit() not working

--- prog4_1.py
+++ prog4_1.py
@@ -12,11 +12,10 @@ def Tokenize(str):
 
 def Parse(tokens):
     for n, i in enumerate(tokens):
-        print(tokens[n])
         if(tokens[n] == "pop" or tokens[n] == "add" or tokens[n] == "sub" or 
             tokens[n] == "mul" or tokens[n] == "div" or tokens[n] == "mod" or 
-            tokens[n] == "skip" and tokens[n][1].isdigit()): True
-        elif(tokens[n] == "push" or tokens[n] == "save" or tokens[n] == "get" or tokens[n].lstrip('-').isdigit()): True
+            tokens[n] == "skip"): True
+        elif((tokens[n] == "push" or tokens[n] == "save" or tokens[n] == "get") or tokens[n].isdigit()): True
         else:
             raise ValueError("Parse error: " + tokens[n])
     return
