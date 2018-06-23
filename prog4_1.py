bd8ae363b1c73c5c96a08463ee23c7361b4b5840
Jeremy Krick <jeremiah.krick@gmail.com>
2018-06-23 11:25:11 -0700

    WIP: Tokenize

--- prog4_1.py
+++ prog4_1.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+class Tokenize():
+    def __init__(self, str):
+        self.str = str
+        for line in str:
+            words = line.split()
+            for n, i in enumerate(words):
+                if (words[n] == "push" or words[n] == "pop" or words[n] == "add" or 
+                    words[n] == "sub" or words[n] == "mul" or words[n] == "div" or 
+                    words[n] == "mod" or words[n] == "skip" or words[n] == "save" or 
+                    words[n] == "get"): True
+                elif words[n].isdigit(): True
+                else:
+                    raise ValueError("Unexpected Token: " + words[n])
--- prog4_3.py
+++ prog4_3.py
@@ -1,1 +1,10 @@
- 
+import sys
+from prog4_1 import Tokenize
+
+def main():
+    with open(sys.argv[1]) as f:
+        data = f.readlines()
+    my_object = Tokenize(data)
+
+if __name__ == '__main__':
+    main()
