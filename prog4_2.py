029dfbb3755eac0d51ec86291d18954457352fc1
Jeremy Krick <jeremiah.krick@gmail.com>
2018-06-24 13:58:52 -0700

    WIP: Execute(tokens)

--- prog4_2.py
+++ prog4_2.py
@@ -62,7 +62,7 @@ class StackMachine:
     def save(self):
         StackMachine.CurrentLine += 1
         var1 = int(self.items.pop())
-        StackMachine.save.append(var1)
+        self.save.append(var1)
 
     def get(self, item):
         StackMachine.CurrentLine += 1
