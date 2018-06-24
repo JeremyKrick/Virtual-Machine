894e9dacc1aa671903b90f428950375da2a5b103
Jeremy Krick <jeremiah.krick@gmail.com>
2018-06-23 22:57:57 -0700

    WIP: Execute()

--- prog4_2.py
+++ prog4_2.py
@@ -1,1 +1,74 @@
- 
+class StackMachine:
+    CurrentLine = 0
+    save = []
+    def __init__(self, items):
+        self.items = []
+        self.Execute(items)
+        #print(items)
+        
+    def push(self, item):
+        self.items.append(item)
+        print(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def add(self):
+        var1 = int(self.items.pop())
+        var2 = int(self.items.pop())
+        var3 = var1 + var2
+        self.push(var3)
+
+    def sub(self):
+        var1 = int(self.items.pop())
+        var2 = int(self.items.pop())
+        var3 = var1 - var2
+        self.push(var3)
+    
+    def mul(self):
+        var1 = int(self.items.pop())
+        var2 = int(self.items.pop())
+        var3 = var1 * var2
+        self.push(var3)
+
+    def div(self):
+        var1 = int(self.items.pop())
+        var2 = int(self.items.pop())
+        var3 = var1 / var2
+        self.push(var3)
+
+    def mod(self):
+        var1 = int(self.items.pop())
+        var2 = int(self.items.pop())
+        var3 = var1 % var2
+        self.push(var3)
+    """
+    def skip(self):
+        var1 = int(self.items.pop())
+        var2 = int(self.items.pop())
+        if(var1 == '0'):
+            CurrentLine = var2
+        
+        var3 = var1 * var2
+        self.push(var3)
+    """
+    def save(self, item):
+        var1 = int(self.items.pop())
+        save.append(var1)
+    """
+    def get(self, item):
+        var1 = int(self.items.pop())
+        var2 = int(self.items.pop())
+        var3 = var1 * var2
+        self.push(var3)
+    """
+    def Execute(self, tokens):
+        for n, i in enumerate(tokens):
+            if(tokens[n][0] == "push"):
+                self.push(tokens[n][1])
+            elif(tokens[n][0] == "pop"):
+                self.pop()
+            elif(tokens[n][0] == "add"):
+                self.add()
+            elif(tokens[n][0] == "sub"):
+                self.sub()
--- prog4_3.py
+++ prog4_3.py
@@ -1,16 +1,20 @@
 import sys
 from prog4_1 import Tokenize
 from prog4_1 import Parse
+from prog4_2 import StackMachine
 
 def main():
     validTokens = []
     with open(sys.argv[1]) as f:
         for line in f:
             validTokens.append(Tokenize(line))
+    f.close
+    #print(validTokens)
     for tokens in validTokens: 
         Parse(tokens)
         #print(Parse(tokens))
-        
+    sm1 = StackMachine(validTokens)
+    #sm1.Execute()
 
 if __name__ == '__main__':
     main()
