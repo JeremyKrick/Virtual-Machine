9b3c7566a690556cf3b18690cc5e90432bc54eee
Jeremy Krick <jeremiah.krick@gmail.com>
2018-06-24 13:49:39 -0700

    WIP: Execute(tokens)

--- prog4_2.py
+++ prog4_2.py
@@ -7,61 +7,67 @@ class StackMachine:
         #print(items)
         
     def push(self, item):
+        StackMachine.CurrentLine += 1
         self.items.append(item)
-        print(item)
+        #print(item)
 
     def pop(self):
+        StackMachine.CurrentLine += 1
         return self.items.pop()
     
     def add(self):
+        StackMachine.CurrentLine += 1
         var1 = int(self.items.pop())
         var2 = int(self.items.pop())
         var3 = var1 + var2
         self.push(var3)
 
     def sub(self):
+        StackMachine.CurrentLine += 1
         var1 = int(self.items.pop())
         var2 = int(self.items.pop())
         var3 = var1 - var2
         self.push(var3)
     
     def mul(self):
+        StackMachine.CurrentLine += 1
         var1 = int(self.items.pop())
         var2 = int(self.items.pop())
         var3 = var1 * var2
         self.push(var3)
 
     def div(self):
+        StackMachine.CurrentLine += 1
         var1 = int(self.items.pop())
         var2 = int(self.items.pop())
         var3 = var1 / var2
         self.push(var3)
 
     def mod(self):
+        StackMachine.CurrentLine += 1
         var1 = int(self.items.pop())
         var2 = int(self.items.pop())
         var3 = var1 % var2
         self.push(var3)
-    """
+
     def skip(self):
         var1 = int(self.items.pop())
         var2 = int(self.items.pop())
         if(var1 == '0'):
-            CurrentLine = var2
-        
-        var3 = var1 * var2
-        self.push(var3)
-    """
-    def save(self, item):
+            StackMachine.CurrentLine += var2
+        else:
+            StackMachine.CurrentLine += 1
+    
+    def save(self):
+        StackMachine.CurrentLine += 1
         var1 = int(self.items.pop())
-        save.append(var1)
-    """
+        StackMachine.save.append(var1)
+
     def get(self, item):
-        var1 = int(self.items.pop())
-        var2 = int(self.items.pop())
-        var3 = var1 * var2
-        self.push(var3)
-    """
+        StackMachine.CurrentLine += 1
+        var1 = StackMachine.save[-1]
+        self.push(var1)
+    
     def Execute(self, tokens):
         for n, i in enumerate(tokens):
             if(tokens[n][0] == "push"):
@@ -72,3 +78,15 @@ class StackMachine:
                 self.add()
             elif(tokens[n][0] == "sub"):
                 self.sub()
+            elif(tokens[n][0] == "mul"):
+                self.mul()
+            elif(tokens[n][0] == "div"):
+                self.div()
+            elif(tokens[n][0] == "mod"):
+                self.mod()
+            elif(tokens[n][0] == "skip"):
+                self.skip()
+            elif(tokens[n][0] == "save"):
+                self.save()
+            elif(tokens[n][0] == "get"):
+                self.get()
